0e6e91bde151d9710a2e5bb483c9c13dd475e7d7
vonny <pochati@yandex.ru>
2015-06-12 11:19:03 +0300

    Dobavili staticPages conroller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,6 +9,7 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publiclyrequire 'securerandom'
+require 'securerandom'
 
 def secure_token
   token_file = Rails.root.join('.secret')
@@ -23,4 +24,6 @@ def secure_token
   end
 end
 
-SampleApp::Application.config.secret_key_base = secure_token.
+
+SampeApp::Application.config.secret_key_base = secure_token
+
