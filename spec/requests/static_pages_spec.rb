7c577b31366435995a5f6c32462900b8e51be8e2
vonny <pochati@yandex.ru>
2015-06-14 22:03:46 +0300

    Finish layout routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,53 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
- let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page" do
-    it "should have the conten 'Sampe App' " do
-       visit '/static_pages/home'
-        expect(page).to have_content('Sampe App')
-    end
-    it "должен быть заголовок 'Home' " do
-     visit "/static_pages/home"
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-    it "Не должен иметь кастом заголовок" do
-     visit "/static_pages/home" 
-      expect(page).not_to have_title('| Home')
-    end
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
+
   describe "Help page" do
-	it "should have the content 'Help' " do
-	 visit '/static_pages/help'
-	 expect(page).to have_content('Help')
-	end
-	 it "должен быть заголвок 'Help' " do
-     visit "/static_pages/help"
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    before { visit help_path }
 
- end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
   describe "About page" do
-	it "should have the content 'About Us'"  do
-	 visit "/static_pages/about"
-	 expect(page).to have_content('About Us')
-	end
-	 it "должен быть заголвок 'About us' " do
-     visit "/static_pages/about"
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    before { visit about_path }
 
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
-   describe "Contact page" do
-  it "должно иметь содержание 'Contact'" do
-   visit "/static_pages/contact"
-   expect(page).to have_content('Contact')
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
- it "должен иметь заголовок 'Contact'" do
-  visit "/static_pages/contact"
-  expect(page).to have_title("#{base_title} | Contact")
-  end
- end
-end 
+end
